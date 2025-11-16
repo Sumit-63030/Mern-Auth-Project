84a331ed6a356352e2b15700918382f442d711d3
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2025-11-16 21:10:38 +0530

    Fix MongoDB connection for Render deployment

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
-    mongoose.connection.on('connected' , () => console.log("Database Connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`)
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 };
 
 export default connectDB;
